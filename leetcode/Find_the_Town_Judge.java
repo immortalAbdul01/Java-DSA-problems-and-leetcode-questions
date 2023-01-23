81ee01c5b4aa88db9023d0d4348f09a4c2fc474f
Immortal <ak2061435@gmail.com>
2023-01-23 10:15:06 +0530

    solved in better approach

--- leetcode/Find_the_Town_Judge.java
+++ leetcode/Find_the_Town_Judge.java
@@ -46,4 +46,20 @@ public class Find_the_Town_Judge {
         }
             return judge;
     }
+
+    // Better approach
+    public int findJudgeBetter(int n, int[][] trust) {
+        if (trust.length == 0 && n == 1)
+            return 1;
+        int[] count = new int[n + 1];
+        for (int[] person : trust) {
+            count[person[0]]--;
+            count[person[1]]++;
+        }
+
+        for (int person = 0; person < count.length; person++) {
+            if (count[person] == n - 1) return person;
+        }
+        return -1;
+    }
 }
